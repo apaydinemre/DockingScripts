ede1c6af840c2eaefd92e26a4169c4572d5e1de2
Emre Apaydın <44448893+apaydinemre@users.noreply.github.com>
2024-01-04 10:44:58 +0300

    Ligand Preparation
    Added two scripts for ligand preparation.

---
+++ rdkit_3d.py
@@ -0,0 +1,41 @@
+from rdkit import Chem
+from rdkit.Chem import rdForceFieldHelpers
+from rdkit.Chem import rdPartialCharges
+import os
+
+
+ligands_dir = "ligands"
+output_dir = "new_ligands"
+status_file = "process_status.txt"
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+sdf_files = [f for f in os.listdir(ligands_dir) if f.endswith(".sdf")]
+
+with open(status_file, 'w') as status:
+    for sdf_file in sdf_files:
+        input_path = os.path.join(ligands_dir, sdf_file)
+        output_path = os.path.join(output_dir, sdf_file)
+        mol = Chem.MolFromMolFile(input_path)
+
+        # Add hydrogens
+        try:
+            mol = Chem.AddHs(mol, addCoords=True)
+        except:
+            status.write(f"{sdf_file} : Chem.AddHs(mol) = Failed\\n")
+            continue
+
+        # Compute Gasteiger charges
+        try:
+            rdPartialCharges.ComputeGasteigerCharges(mol)
+        except:
+            status.write(f"{sdf_file} : rdPartialCharges.ComputeGasteigerCharges(mol) = Failed\\n")
+
+        # UFF energy minimization
+        try:
+            rdForceFieldHelpers.UFFOptimizeMolecule(mol)
+        except:
+            status.write(f"{sdf_file} : rdForceFieldHelpers.UFFOptimizeMolecule(mol) = Failed\\n")
+
+        Chem.MolToMolFile(mol, output_path)
